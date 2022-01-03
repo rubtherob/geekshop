57b6979f449942e8e0e00a96ef3293c16fe5f562
Dima <rubtherobs@gmail.com>
2022-01-03 14:22:06 +0300

    Part 2 Lesson 3

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -150,7 +150,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 AUTH_USER_MODEL = 'authapp.User'
 

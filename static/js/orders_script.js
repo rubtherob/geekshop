820452bd6ab3ac8384a1eb547c9949522507f50d
Dima <rubtherobs@gmail.com>
2022-01-04 00:37:04 +0300

    update

--- static/js/orders_script.js
+++ static/js/orders_script.js
@@ -1,32 +1,29 @@
 window.onload = function () {
-    var _quantity, _price, orderitem_num, delta_quantity, orderitem_quantity, delta_cost;
-    var quantity_arr = [];
-    var price_arr = [];
 
-    var TOTAL_FORMS = parseInt($('input[name="orderitems-TOTAL_FORMS"]').val());
+    let quantity, price, orderitem_num, delta_quantity, orderitem_quantity, delta_cost;
 
-    var order_total_quantity = parseInt($('.order_total_quantity').text()) || 0;
-    var order_total_cost = parseFloat($('.order_total_cost').text().replace(',', '.')) || 0;
+    let quantity_arr = []
+    let price_arr = []
+    let total_forms = parseInt($('input[name=orderitems-TOTAL_FORMS]').val())
 
-    for (var i = 0; i < TOTAL_FORMS; i++) {
-        _quantity = parseInt($('input[name="orderitems-' + i + '-quantity"]').val());
-        _price = parseFloat($('.orderitems-' + i + '-price').text().replace(',', '.'));
-        quantity_arr[i] = _quantity;
-        if (_price) {
-            price_arr[i] = _price;
+    let order_total_quantity = parseInt($('.order_total_quantity').text()) || 0;
+    let order_total_cost = parseInt($('.order_total_cost').text().replace(',', '.')) || 0;
+
+    for (let i = 0; i < total_forms; i++) {
+        quantity = parseInt($('input[name=orderitems-' + i + '-quantity]').val())
+        price = parseInt($('.orderitems-' + i + '-price').text().replace(',', '.'))
+
+        quantity_arr[i] = quantity;
+        if (price) {
+            price_arr[i] = price
         } else {
             price_arr[i] = 0;
         }
     }
-    if (!order_total_quantity) {
-        for (var i = 0; i < TOTAL_FORMS; i++) {
-            order_total_quantity += quantity_arr[i];
-            order_total_cost += quantity_arr[i] * price_arr[i];
-        }
-        $('.order_total_quantity').html(order_total_quantity.toString());
-        $('.order_total_cost').html(Number(order_total_cost.toFixed(2)).toString());
-
-    }
+    console.info('QUANTITY', quantity_arr)
+    console.info('PRICE', price_arr)
+    //
+    // 1метод
     $('.order_form').on('click', 'input[type=number]', function () {
 
         let target = event.target;
@@ -39,6 +36,7 @@ window.onload = function () {
         }
     });
 
+    // 2 метод
     $('.order_form').on('click', 'input[type=checkbox]', function () {
 
         let target = event.target;
@@ -75,8 +73,8 @@ window.onload = function () {
         orderSummaryUpdate(price_arr[orderitem_num], delta_quantity)
     }
 
-    // $('.order_form select').change(function () {
-    $(document).on('change', '.order_form select', function (){
+    $('.order_form select').change(function () {
+
         let target = event.target;
         orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-product', ''));
         let orderitem_product_pk = target.options[target.selectedIndex].value;
@@ -105,4 +103,37 @@ window.onload = function () {
         }
 
     })
+
+    // $('.order_form select').change(function () {
+    $(document).on('change', '.order_form select', function () {
+
+        let target = event.target;
+        orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-product', ''));
+        let orderitem_product_pk = target.options[target.selectedIndex].value;
+
+        console.log(orderitem_num)
+        console.log(orderitem_product_pk)
+
+        if (orderitem_product_pk) {
+            $.ajax({
+                url: '/orders/product/' + orderitem_product_pk + '/price/',
+                success: function (data) {
+                    if (data.price) {
+                        price_arr[orderitem_num] = parseFloat(data.price)
+                        if (isNaN(quantity_arr[orderitem_num])) {
+                            quantity_arr[orderitem_num] = 0;
+                        }
+                        let price_html = '<span class="orderitems-' + orderitem_num + '-price">'
+                            + data.price.toString().replace('.', ',') + '</span> руб';
+                        let current_tr = $('.order_form table').find('tr:eq(' + (orderitem_num + 1) + ')');
+                        current_tr.find('td:eq(2)').html(price_html)
+                    }
+                }
+
+
+            })
+        }
+
+    })
+
 }

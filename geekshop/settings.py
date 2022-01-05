4b840178a080d36cb2a9a01e8b23cf74a4c3329b
Dima <rubtherobs@gmail.com>
2022-01-05 12:35:05 +0300

    update

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'ordersapp',
     'debug_toolbar',
     'template_profiler_panel',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -144,8 +145,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 #STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, "static"),
-# )
+#   os.path.join(BASE_DIR, "static"),
+#)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -163,7 +164,7 @@ LOGIN_REDIRECT_URL='index'
 LOGIN_ERROR_URL = '/'
 
 
-DOMAIN_NAME = 'http://80.78.241.58/'
+# DOMAIN_NAME = 'http://80.78.241.58/'
 
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = '465'
